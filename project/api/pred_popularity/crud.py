db0fd995ebc0fc8e45be7414757c10bed1aa249c
C. Ahn <moz5691@gmail.com>
2020-08-02 15:48:01 -0400

    ann model update, popularity prediction crud update

--- project/api/pred_popularity/crud.py
+++ project/api/pred_popularity/crud.py
@@ -1,6 +1,10 @@
 import os
+import json
+import numpy as np
 from pathlib import Path
 import joblib
+from tensorflow.keras.models import load_model
+### retreive model and weight
 
 MODEL_PATH = os.path.join(Path(__file__).parent.parent, 'models')
 
@@ -11,12 +15,72 @@ MODEL_PATH = os.path.join(Path(__file__).parent.parent, 'models')
 #     all_lines = f.readlines()
 #     print(all_lines)
 
+model = load_model(f'{MODEL_PATH}/ann_pop_model.h5')
+scaler = joblib.load(f'{MODEL_PATH}/ann_pop_scaler.pkl')
 
-def predict_popularity(acousticness, danceability, duration_ms, energy,
-                       instrumentalness):
+genre_conversion = {
+    'Alternative': 7,
+    'Anime': 8,
+    'Blues': 9,
+    'Childrens': 10,
+    'Classical': 11,
+    'Comedy': 12,
+    'Country': 13,
+    'Dance': 14,
+    'Electronic': 15,
+    'Folk': 16,
+    'Hip-Hop': 17,
+    'Indie': 18,
+    'Jazz': 19,
+    'Movie': 20,
+    'Opera': 21,
+    'Pop': 22,
+    'R&B': 23,
+    'Rap': 24,
+    'Reggae': 25,
+    'Reggaeton': 26,
+    'Rock': 27,
+    'Ska': 28,
+    'Soul': 29,
+    'Soundtrack': 30,
+    'World': 31
+}
 
-    ## implement here.
-    fixed = 123
-    print(acousticness, danceability, duration_ms, energy, instrumentalness)
 
-    return float(fixed)
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def predict_popularity(acousticness, danceability, energy, instrumentalness,
+                       liveness, loudness, speechiness, genre):
+
+    X_test = np.array([[
+        acousticness, danceability, energy, instrumentalness, liveness,
+        loudness, speechiness, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0
+    ]])
+
+    index = int(genre_conversion[genre])
+
+    if (index > 6 and index < 32):
+        # choose genre here
+        X_test[0][index] = 1
+    else:
+        # if not in range, choose 'Pop' as genre
+        X_test[0][22] = 1
+
+    print(acousticness, danceability, energy, instrumentalness, liveness,
+          loudness, speechiness, genre)
+
+    print('X_test --> ', X_test)
+
+    X_scaled = scaler.transform(X_test)
+    predict = model.predict(X_scaled)
+    arg_max = np.argmax(predict)
+    print('arg_max -->', arg_max)
+
+    return (predict[0].tolist(), int(arg_max))
--- project/api/pred_popularity/views.py
+++ project/api/pred_popularity/views.py
@@ -1,24 +1,34 @@
-# popularity
-# acousticness
-# danceability
-# duration_ms
-# energy
-# instrumentalness
-
-from project.api.pred_popularity.crud import (predict_popularity)
-
 from flask import request
 from flask_restx import Resource, Api, fields, Namespace
 
-pred_popularity_namespace = Namespace("pred_popularity")
+from project.api.pred_popularity.crud import predict_popularity
+
+pred_popularity_namespace = Namespace(
+    "pred_popularity", description="Data Input for Popularity Prediction")
 
 pred_popularity = pred_popularity_namespace.model(
     'Pred_Popularity', {
-        'acousticness': fields.Float(),
-        'danceability': fields.Float(),
-        'duration_ms': fields.Float(),
-        'energy': fields.Float(),
-        'instrumentalness': fields.Float(),
+        'acousticness':
+        fields.Float(required=True, description="Float, Min:0, Max:1"),
+        'danceability':
+        fields.Float(required=True, description="Float, Min:0, Max:1"),
+        'energy':
+        fields.Float(required=True, description="Float, Min:0, Max:1"),
+        'instrumentalness':
+        fields.Float(required=True, description="Float, Min:0, Max:1"),
+        'liveness':
+        fields.Float(required=True, description="Float, Min:0, Max:1"),
+        'loudness':
+        fields.Float(required=True,
+                     description="Float, Min:-52, Max:3.8, unit dB"),
+        'speechiness':
+        fields.Float(required=True, description="Float, Min:0, Max:1"),
+        'genre':
+        fields.String(
+            required=True,
+            description=
+            "String, [Alternative,Anime,Blues,Childrens,Classical,Comedy,Country,Dance,Electronic,Folk,Hip-Hop,Indie,Jazz,Movie,Opera,Pop,R&B,Rap,Reggae,Reggaeton,Rock,Ska,Soul,Soundtrack,World]"
+        ),
     })
 
 
@@ -30,14 +40,25 @@ class PredPopularity(Resource):
         post_data = request.get_json()
         acousticness = post_data.get("acousticness")
         danceability = post_data.get("danceability")
-        duration_ms = post_data.get("duration_ms")
         energy = post_data.get("energy")
         instrumentalness = post_data.get("instrumentalness")
+        liveness = post_data.get("liveness")
+        loudness = post_data.get("loudness")
+        speechiness = post_data.get("speechiness")
+        genre = post_data.get("genre")
         response_object = {}
 
-        response = predict_popularity(acousticness, danceability, duration_ms,
-                                      energy, instrumentalness)
-        response_object = {'pouplarity': response}
+        response = predict_popularity(acousticness=acousticness,
+                                      danceability=danceability,
+                                      energy=energy,
+                                      instrumentalness=instrumentalness,
+                                      liveness=liveness,
+                                      loudness=loudness,
+                                      speechiness=speechiness,
+                                      genre=genre)
+
+        print('response--->', response)
+        response_object = {'probability': response[0], 'class': response[1]}
         return response_object, 201
 
 
